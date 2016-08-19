b4a94dfb17760364f36ba40106929a98912c3c2e
FahimaSulthana <fahitweety@gmail.com>
2016-08-19 15:51:19 +0530

    Update vowelorconsonant.java

--- vowelorconsonant.java
+++ vowelorconsonant.java
@@ -1,23 +1,24 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class vowelorconsonant
+
+class Ideone
 {
- public static void Main(String[] args)
- {
-  int c=0;
-  char input;
-  char[] vowels={'a','e','i','o','u'};
-  Scanner sc=new Scanner(System.in);
-  input=sc.next().charAt(0);
-  for(int i=0;i<5;i++)
-  {
-   if(vowels[i].equals(input))
-    c++;
-  }
-  if(c!=0)
-   System.out.println("its a vowel");
-  else
-   System.out.println("its a consonqnt");
- }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		      int c=0;
+        String input;
+        String[] vowels={"a","e","i","o","u"};
+        Scanner sc=new Scanner(System.in);
+        input=sc.next();
+        for(int i=0;i<5;i++)
+        {
+         if(vowels[i].equals(input))
+         c++;
+        }
+        if(c!=0)
+         System.out.println("its a vowel");
+        else
+         System.out.println("its a consonant");
+	}
 }
