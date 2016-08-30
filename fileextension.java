fa4c26899dc8f20e0fe7ce729ea422f7024a64f8
FahimaSulthana <fahitweety@gmail.com>
2016-08-30 17:28:20 +0530

    Create fileextension.java

---
+++ fileextension.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class fileextension
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+		int flag=0;+
+		char[] a=s.toCharArray();
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]=='.')
+			{
+				flag=1;
+				i++;
+			}
+			if(flag==1)
+				System.out.println(a[i]);
+
+			
+		}
+
+	}
+}
