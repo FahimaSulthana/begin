5e929bb25bcb2ca20d9e9bd11cd67b34109de2f5
FahimaSulthana <fahitweety@gmail.com>
2016-08-30 14:45:01 +0530

    Create frstdigitshdbedigit.java

---
+++ frstdigitshdbedigit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+		Boolean b;
+		s.toCharArray();
+		b=Character.isDigit(s.charAt(0));
+		System.out.println(b);
+		
+	}
+}
