62b98c99cb4daac14d2c9eb704d2a680d1ab0615
FahimaSulthana <fahitweety@gmail.com>
2016-08-31 17:33:49 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class compare
+{
+	public static void main(String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		if(s1.equals(s2))
+		{
+			System.out.println("equal");
+		}
+		else
+			System.out.println("not equal");
+	}
+}
